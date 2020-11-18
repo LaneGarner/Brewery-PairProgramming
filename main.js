a13aca0fab6b356b2488b2e361a2c41a0dca408c
nblack0917 <nickablack@gmail.com>
2020-11-17 21:57:48 -0600

    build DOM and link code

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ window.onload = function() {
         getBreweries()
     }, 5000)
 
-    setTimeout(()=> {
-        checkForNear()
-    }, 6000)
+    // setTimeout(()=> {
+    //     checkForNear()
+    // }, 6000)
 }
 
 const getCity = () => {
@@ -27,34 +27,54 @@ const getCity = () => {
     .then(response => {
         myCity = response.results[0].locations[0].adminArea5
         console.log(myCity)
+        document.getElementById("curLocation").innerHTML = myCity;
     })
 }
 
 const getBreweries = () => {
+    // myCity = "austin"
     fetch(`https://api.openbrewerydb.org/breweries?by_city=${myCity}&per_page=50`)
     .then(res => res.json())
     .then(response => {
         breweriesPage1 = response;
     })
-    fetch(`https://api.openbrewerydb.org/breweries?by_city=${myCity}&per_page=50&page=2`)
-    .then(res => res.json())
-    .then(response => {
-        breweriesPage2 = response;
-        allBreweries = breweriesPage1.concat(breweriesPage2)
-        console.log(allBreweries)
-    })
     .catch(err => {
         console.error(err);
     });
+    setTimeout(() => {
+        if (breweriesPage1.length === 50) {
+            fetch(`https://api.openbrewerydb.org/breweries?by_city=${myCity}&per_page=50&page=2`)
+            .then(res => res.json())
+            .then(response => {
+                breweriesPage2 = response;
+                    allBreweries = breweriesPage1.concat(breweriesPage2)
+                    console.log(allBreweries)
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        } else {
+            allBreweries = breweriesPage1;
+        }
+    }, 500)
+    
+    setTimeout(() => {
+        let numOfBrews = allBreweries.length;
+        if (numOfBrews > 0) {
+            document.getElementById("numOfBreweries").innerHTML = `There are <br>${numOfBrews} breweries in your city.`
+        } else {
+            document.getElementById("numOfBreweries").innerHTML = `Oh no!<br>There are ${numOfBrews} breweries in your city. Try searching a nearby city.`
+        }
+    }, 600)
 }
 
-const checkForNear = () => {
+const checkForNear = (dist) => {
     for (let brewery of allBreweries) {
         if (brewery.latitude !== null) {
             let diffLat = Math.abs(brewery.latitude-currentLat)
-            if (diffLat <= .144927) {
+            if (diffLat <= dist) {
                 let diffLong = Math.abs(brewery.longitude-currentLong)
-                if (diffLong <= .144927) {
+                if (diffLong <= dist) {
                     nearByBrews.push(brewery)
                 }
             }
@@ -63,24 +83,90 @@ const checkForNear = () => {
     console.log(nearByBrews)
 }
 
-
-
-//CHECK IF BREW HAS COORDS
-// IF NO COORDS .. GET ADDRESS AND USE GET COORDS FUNCTION BELOW
-// PUSH COORDINATES INTO 
-
-
-
-const getCoords = () => {
-    fetch("https://www.mapquestapi.com/geocoding/v1/address?key=IOGuXL0zAKHaQVwYf9qGnm4UQm9ZG7PZ&inFormat=kvp&outFormat=json&location=4602+Weletka+Dr+austin+tx&thumbMaps=false")
+const getCoords = (city) => {
+    fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=8q0FAWlpVLSby4A5G8GyjZliyncLL3Wh&inFormat=kvp&outFormat=json&location=${city}&thumbMaps=false`)
     .then(res => res.json())
     .then(response => {
         coords = response;
         console.log(coords.results[0].locations[0].displayLatLng)
+        currentLat = coords.results[0].locations[0].displayLatLng.lat
+        currentLong = coords.results[0].locations[0].displayLatLng.lng
     })
 }
 
-getCoords()
+const updateCity = () => {
+    console.log("click button")
+    let input = document.getElementById("citySearch").value
+    myCity = input.toLowerCase();
+    breweriesPage1 = null;
+    breweriesPage2 = null;
+    allBreweries = null;
+    nearByBrews = [];
+    console.log(myCity)
+    getBreweries();
+    getCoords(myCity)
+    setTimeout(() => {
+        checkForNear()
+    }, 2000)
+}
+
+const searchDistance = () => {
+    let input = document.getElementById("distance").value
+    if (input === "five") {
+        nearByBrews = [];
+        checkForNear(0.0724637681309405)
+    } else if (input === "ten") {
+        nearByBrews = [];
+        checkForNear(0.144927536261881)
+    } else if (input === "twenty") {
+        nearByBrews = [];
+        checkForNear(0.289855072523762)
+    } else if (input === "fifty") {
+        nearByBrews = [];
+        checkForNear(1)
+    }
+    const brewList = document.getElementById("searchResults")
+    brewList.innerHTML = null;
+    nearByBrews.map((brews) => {
+        const li = document.createElement('li');
+        const text = document.createTextNode(`${brews.name} - Brew Type: ${brews.brewery_type} - ${brews.street} ${brews.city}`)
+        li.appendChild(text)
+        brewList.appendChild(li)
+    })
+}
+
+//CHECK IF BREW HAS COORDS
+// IF NO COORDS .. GET ADDRESS AND USE GET COORDS FUNCTION BELOW
+// PUSH COORDINATES INTO 
+// const checkForCoords = () => {
+//     let newLat;
+//     let lewLong;
+//     for(let brewery of allBreweries) {
+//         if (brewery.latitude === null && brewery.street !== "") {
+//             let streetAdd = [];
+//             streetAdd = brewery.street.split(" ")
+//             let newStreet = streetAdd.join("+")
+//             // console.log(newStreet)
+
+//             setTimeout(() => {
+//                 getCoords(newStreet, brewery.city, brewery.state)
+//             }, 5000)
+//         }
+//     }
+// }
+
+
+// const getCoords = (city) => {
+//     fetch(`https://www.mapquestapi.com/geocoding/1/address?key=8q0FAWlpVLSby4A5G8GyjZliyncLL3Wh&inFormat=kvp&outFormat=json&location=${city}&thumbMaps=false`)
+//     .then(res => res.json())
+//     .then(response => {
+//         coords = response;
+//         console.log(coords.results[0].locations[0].displayLatLng)
+//     })
+//     console.log(`${street}+${city}+${state}`)
+// }
+
+// getCoords()
 
 
 //COMPARE USER COORDS TO BREW COORDS
@@ -92,3 +178,5 @@ getCoords()
 
 
 // .0144927536261881 = 1 mile
+
+
