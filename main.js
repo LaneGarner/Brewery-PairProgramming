afe470d3f8eb14e59f3ab9d5617200b4dacc222e
Lane Garner <b.lane.garner@gmail.com>
2020-11-18 22:07:35 -0600

    pushing for the night

--- main.js
+++ main.js
@@ -2,15 +2,6 @@
 
 let nearByBrews = [], favoriteBrews= [], dif = 0.21739130434782608, breweriesPage1, breweriesPage2, allBreweries, coords, city, myCity, currentLat, currentLong;
 
-
-const randomBeerPic = () => {
-    const beerPics =['photos/beer01.jpg', 'photos/beer02.jpg', 'photos/beer03.jpg', 'photos/beer04.jpg', 'photos/beer05.jpg', 'photos/beer06.jpg', 'photos/beer07.jpg', 'photos/beer08.jpg', 'photos/beer09.jpg', 'photos/beer10.jpg', 'photos/beer11.jpg', 'photos/beer12.jpg', 'photos/beer13.jpg', 'photos/beer14.jpg', 'photos/beer15.jpg']
-    let randomBeer = beerPics[Math.floor(Math.random() * beerPics.length)]
-    // let randomBeerSelect = `${randomBeer}"></img>`
-    return randomBeer
-}
-
-
 const getLocation = () => {
     return new Promise((resolve)=>{
 
@@ -40,34 +31,6 @@ const getCity = () => {
             })
     })
 }
-
-
-// const getBreweries = () => {
-//     return new Promise((resolve)=>{
-
-//         fetch(`https://api.openbrewerydb.org/breweries?by_city=${myCity}&per_page=50`)
-//         .then(res => res.json())
-//         .then(response => {
-//             breweriesPage1 = response;
-//         })
-
-//         fetch(`https://api.openbrewerydb.org/breweries?by_city=${myCity}&per_page=50&page=2`)
-//         .then(res => res.json())
-//         .then(response => {
-//             breweriesPage2 = response;
-//             setTimeout(()=>{
-//                 allBreweries = breweriesPage1.concat(breweriesPage2)
-//                 console.log(allBreweries)
-//                 resolve()
-//             }, 500)
-            
-//         })
-//         .catch(err => {
-//             console.error(err);
-//         })
-
-//     })
-// }
 
 const getBreweries = () => {
     return new Promise((resolve)=>{
@@ -128,7 +91,6 @@ const getNewCoords = (street,city,state,brewery) => {
     .then(res => res.json())
     .then(response => {
         coords = response;
-        // console.log(coords.results[0].locations[0].displayLatLng)
         brewery.latitude = coords.results[0].locations[0].displayLatLng.lat
         brewery.longitude = coords.results[0].locations[0].displayLatLng.lng
         console.log(brewery)
@@ -138,25 +100,16 @@ const getNewCoords = (street,city,state,brewery) => {
 const updateCity = () => {
     if (allBreweries.length === 0){
         return new Promise((resolve)=>{
-            
-            console.log('updatecity')
-            myCity = prompt("Hmm... it looks like there are no breweries in your city.", "Enter the name of a nearby city here")
-            // console.log(input)
-            // myCity = input.toLowerCase();
+            myCity = prompt("Hmm... no breweries were found in your city", "Enter the name of a nearby city here")
             breweriesPage1 = null;
             breweriesPage2 = null;
             allBreweries = null;
-            // nearByBrews = [];
             console.log(myCity)
             resolve()
-            // getCoords(myCity)
-            // setTimeout(() => {
-                //     checkForNear()
-                // }, 2000)
-            })
-            .then(getBreweries)
-            .then(getMoreBreweries)
-            .then(getCoords);
+        })
+        .then(getBreweries)
+        .then(getMoreBreweries)
+        .then(getCoords);
     } else {
         return new Promise((resolve)=>{
             resolve()
@@ -164,53 +117,66 @@ const updateCity = () => {
     }
 }
 
-const checkForNear = () => {
-    return new Promise((resolve)=>{
-        console.log(allBreweries)
+// const checkForNear = () => {
+//     return new Promise((resolve)=>{
+//         console.log(allBreweries)
 
-        for (let brewery of allBreweries) {
-            if (brewery.latitude !== null) {
-                let diffLat = Math.abs(brewery.latitude-currentLat)
-                if (diffLat <= dif) {
-                    let diffLong = Math.abs(brewery.longitude-currentLong)
-                    if (diffLong <= dif) {
-                        nearByBrews.push(brewery)
-                    }
-                }
-            }
-        }
-        resolve()
-        console.log(nearByBrews)
-    })
+//         for (let brewery of allBreweries) {
+//             if (brewery.latitude !== null) {
+//                 let diffLat = Math.abs(brewery.latitude-currentLat)
+//                 if (diffLat <= dif) {
+//                     let diffLong = Math.abs(brewery.longitude-currentLong)
+//                     if (diffLong <= dif) {
+//                         nearByBrews.push(brewery)
+//                     }
+//                 }
+//             }
+//         }
+//         resolve()
+//         console.log(nearByBrews)
+//     })
+// }
+ 
+
+
+// const getCoords = () => {
+//     fetch("https://www.mapquestapi.com/geocoding/v1/address?key=IOGuXL0zAKHaQVwYf9qGnm4UQm9ZG7PZ&inFormat=kvp&outFormat=json&location=4602+Weletka+Dr+austin+tx&thumbMaps=false")
+//     .then(res => res.json())
+//     .then(response => {
+//         coords = response;
+//         console.log(coords.results[0].locations[0].displayLatLng)
+//     })
+// }
+
+
+
+const randomBeerPic = () => {
+    const beerPics =['photos/beer01.jpg', 'photos/beer02.jpg', 'photos/beer03.jpg', 'photos/beer04.jpg', 'photos/beer05.jpg', 'photos/beer06.jpg', 'photos/beer07.jpg', 'photos/beer08.jpg', 'photos/beer09.jpg', 'photos/beer10.jpg', 'photos/beer11.jpg', 'photos/beer12.jpg', 'photos/beer13.jpg', 'photos/beer14.jpg', 'photos/beer15.jpg']
+    let randomBeer = beerPics[Math.floor(Math.random() * beerPics.length)]
+    return randomBeer
 }
 
-
-
-
-//CHECK IF BREW HAS COORDS
-// IF NO COORDS .. GET ADDRESS AND USE GET COORDS FUNCTION BELOW
-// PUSH COORDINATES INTO 
-
-
-
-const getCoords = () => {
-    fetch("https://www.mapquestapi.com/geocoding/v1/address?key=IOGuXL0zAKHaQVwYf9qGnm4UQm9ZG7PZ&inFormat=kvp&outFormat=json&location=4602+Weletka+Dr+austin+tx&thumbMaps=false")
-    .then(res => res.json())
-    .then(response => {
-        coords = response;
-        console.log(coords.results[0].locations[0].displayLatLng)
-    })
-}
-
-// getCoords()
-
-
-
-
-const displayNearByBrews = () => {
+const displayBrews = () => {
+    nearByBrews = allBreweries;
     let domBrews = document.querySelector('#dom-brews');
     let listOfBrews = document.createElement('ul');
-    let load = document.querySelector('#load')
+    
+    let load = document.querySelector('#load');
+        load.style.display = 'none';
+
+    let yourCity = document.querySelector('#yourCity');
+        yourCity.innerHTML = `
+            Showing results for ${myCity}<br>
+            ${allBreweries.length} breweries found
+        `
+    // let cityInfo = document.createElement('p');
+    //     cityInfo.innerHTML = `Showing results for ${myCity}`
+    // yourCity.appendChild(cityInfo);
+
+    let citySearch = document.querySelector('#citySearch');
+    citySearch.style.display = "flex";
+
+
     let brewItem;
     console.log(nearByBrews)
     nearByBrews.map((brew, index) => {
@@ -240,8 +206,16 @@ const displayNearByBrews = () => {
     })
 
     domBrews.appendChild(listOfBrews)
-    load.style.display = 'none';
 }
+
+getLocation()
+    .then(getCity)
+    .then(getBreweries)
+    .then(getMoreBreweries)
+    .then(checkForCoords)
+    .then(updateCity)
+    // .then(checkForNear)
+    .then(displayBrews);
 
 
 const likeIt = (elem) => {
@@ -259,14 +233,22 @@ const likeIt = (elem) => {
 }
 
 
-getLocation()
-    .then(getCity)
+
+
+
+const newCity = () => {
+    document.querySelector('#dom-brews').innerHTML = '';
+    myCity = document.querySelector('#search-city').value;
+    return new Promise((resolve)=>{
+        resolve()
+    })
     .then(getBreweries)
     .then(getMoreBreweries)
     .then(checkForCoords)
     .then(updateCity)
-    .then(checkForNear)
-    .then(displayNearByBrews);
+    // .then(checkForNear)
+    .then(displayBrews);
+}
 
 // console.log('All breweries: ', allBreweries)
 // console.log('breweries 2: ', breweries2)
